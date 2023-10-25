cb00f2417c3b928f2b162700c5db5edb73a46af2
Salman Budhwani <sbudhwani@logitech.com>
2023-10-25 15:43:55 -0700

    Update UrlValidator.php

--- lib/PayPal/Validation/UrlValidator.php
+++ lib/PayPal/Validation/UrlValidator.php
@@ -19,7 +19,7 @@ class UrlValidator
      */
     public static function validate($url, $urlName = null)
     {
-        if (filter_var($url, FILTER_VALIDATE_URL) === false) {
+        if (!is_null($url) && !empty($url) && filter_var($url, FILTER_VALIDATE_URL) === false) {
             throw new \InvalidArgumentException("$urlName is not a fully qualified URL");
         }
     }
